531a2dd5f2c603eaae0a3b4ff4ad53cea5328e25
education20172017@gmail.com <Ltrf,hm_2018>
2019-07-30 00:31:36 +0300

    Added dispatcher - still not working

--- src/main/java/com/example/xmljmsSender/GreetingController.java
+++ src/main/java/com/example/xmljmsSender/GreetingController.java
@@ -3,9 +3,12 @@ package com.example.xmljmsSender;
 import com.example.xmljmsSender.jms.MessageToQueue;
 import com.example.xmljmsSender.jms.Sender;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.Servlet;
 
 @Controller
 public class GreetingController {
---
+++ src/main/java/com/example/xmljmsSender/HelloController.java
@@ -0,0 +1,26 @@
+package com.example.xmljmsSender;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class HelloController extends AbstractController {
+
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+                                                 HttpServletResponse response) throws Exception {
+
+        ModelAndView model = new ModelAndView("HelloWorldPage");
+        model.addObject("msg", "hello world");
+
+        return model;
+    }
+
+}
